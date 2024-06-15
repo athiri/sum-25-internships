361793e59bc07beabdb5f005c1bd23e89c1faab6
Mark <42273752+athiri@users.noreply.github.com>
2024-06-15 03:06:11 -0700

    update readme

--- .github/scripts/update_readmes.py
+++ .github/scripts/update_readmes.py
@@ -9,19 +9,19 @@ def main():
     # split up listings into summer and off-season
     # TODO: add new grads
     summer_listings = [listing for listing in listings if listing["season"] == "Summer"]
-    offseason_listings = [listing for listing in listings if listing["season"] != "Summer"]
+    #offseason_listings = [listing for listing in listings if listing["season"] != "Summer"]
 
     # validate listings
     util.checkSchema(summer_listings)
-    util.checkSchema(offseason_listings)
+    #util.checkSchema(offseason_listings)
 
     # sort listings
     summer_listings = util.sortListings(summer_listings) # no longer in place my bad :(
-    offseason_listings = util.sortListings(offseason_listings)
+    #offseason_listings = util.sortListings(offseason_listings)
 
     # create table and embed
     util.embedTable(summer_listings, "README.md")
-    util.embedTable(offseason_listings, "OFFSEASON_README.md")
+    #util.embedTable(offseason_listings, "OFFSEASON_README.md")
 
     util.setOutput("commit_message", "Updating READMEs at " + datetime.now().strftime("%B %d, %Y %H:%M:%S"))
 
